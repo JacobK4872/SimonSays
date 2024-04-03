98822b71b30549d6bb78d4c4a093394f10d68088
Jacob <jacobkellogg17@gmail.com>
2024-04-03 16:39:00 -0500

    initial commit

---
+++ js/simon.js
@@ -0,0 +1,19 @@
+// export default function simon() {
+//     let simonCommands = ['jump', 'wave', 'sit'];
+//     let randomCommand = simonCommands[Math.floor(math.random() * simonCommands.length)];
+//     console.log(randomCommand);
+// }
+//     return(
+//     `
+//         <div class="randomCommand">
+//             ${randomCommand}
+//         <div>
+//     `);
+// }
+
+
+
+
+
+
+
